8bdda1ff412c595c959018bfb5b0610c7a40e784
Snr Andy <nfonandrew73@gmail.com>
2024-12-13 14:38:11 +0100

    change base url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://test.nfonandrew.info/';
+axios.defaults.baseURL = 'https://test.nfonandrew.info';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
